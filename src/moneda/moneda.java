a08ddc95b4bfe19c9563e7595d4751f452a05cb6
Raos <raos.fast@gmail.com>
2018-05-23 00:12:24 -0400

    los datos de un registro se guarda en un objeto moneda

--- src/moneda/moneda.java
+++ src/moneda/moneda.java
@@ -5,15 +5,40 @@ public class moneda {
     private String moneda;
     private int valor;
     private String dia;
+    private int valorTotal;
+    private int valorConversion;
     
- 
-    //Constructor
+  //Constructor
+    public moneda() {
+        
+    }
+    
     public moneda(String conversion, String moneda, int valor, String dia) {
         this.conversion = conversion;
         this.moneda = moneda;
         this.valor = valor;
         this.dia = dia;
     }
+    
+ 
+    public int getValorTotal() {
+		return valorTotal;
+	}
+
+
+	public void setValorTotal(int valorTotal) {
+		this.valorTotal = valorTotal;
+	}
+
+
+	public int getValorConversion() {
+		return valorConversion;
+	}
+
+
+	public void setValorConversion(int valorConversion) {
+		this.valorConversion = valorConversion;
+	}
 
 
 	public String getConversion() {
